d135d456d5c190b27c824713919394e211fc7098
Sehajdeep Singh <ssingh28_be24@thapar.edu>
2026-01-24 02:29:21 +0530

    remove frontend static serving for Render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
 
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/route.js";
@@ -9,28 +8,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	const frontendPath = path.resolve(__dirname, "../frontend/dist");
-
-	app.use(express.static(frontendPath));
-
-	app.get("/*", (req, res) => {
-		res.sendFile(path.join(frontendPath, "index.html"));
-	});
-}
-
-
 const startServer = async () => {
 	try {
 		await connectDB();
-
 		app.listen(PORT, () => {
-			console.log(`Server started at http://localhost:${PORT}`);
+			console.log(`Server started on port ${PORT}`);
 		});
 	} catch (error) {
 		console.error("Failed to start server:", error.message);
